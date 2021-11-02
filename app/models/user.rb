58279c08cc3abe0267c553bacd1bab9b2a00d24d
DEVU R KURUP <devukurup199918@gmail.com>
2021-11-03 02:51:38 +0530

    Included constraints to user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    validates :email, presence: true
+    validates :first_name, presence: true, length: { maximum: Constants::MAX_LENGTH }
+    validates :last_name, presence: true, length: { maximum: Constants::MAX_LENGTH }
+    before_save :to_lowercase
+
+    private
+        def to_lowercase
+            email.downcase!
+        end
 end
