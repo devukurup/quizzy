25e33daf97f7a60f9b9260c85757baf77a849172
DEVU R KURUP <devukurup199918@gmail.com>
2021-11-03 15:55:21 +0530

    Added some negative test cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,6 +9,12 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  def test_invalid_user
+    @user.first_name = ""
+    assert_not @user.valid?
+    assert_includes @user.errors.full_messages, "First name can't be blank"
+  end
+
   def test_user_should_not_be_valid_and_saved_without_first_name
     @user.first_name = ''
     assert_not @user.valid?
@@ -33,10 +39,20 @@ class UserTest < ActiveSupport::TestCase
     assert_includes @user.errors.full_messages, "First name is too long (maximum is 50 characters)"
   end
 
+  def test_first_name_valid_with_valid_length
+    @user.first_name = 'a' * 49
+    assert @user.valid?
+  end
+
   def test_last_name_should_be_of_valid_length
     @user.last_name = 'a' * 51
     assert_not @user.valid?
     assert_includes @user.errors.full_messages, "Last name is too long (maximum is 50 characters)"
+  end
+
+  def test_last_name_valid_with_valid_length
+    @user.last_name = 'a' * 49
+    assert @user.valid?
   end
 
   def test_user_should_not_be_valid_and_saved_if_email_not_unique
@@ -45,7 +61,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not test_user.valid?
     assert_includes test_user.errors.full_messages, "Email has already been taken"
   end
-  
+
   def test_email_should_be_saved_in_lowercase
     uppercase_email = "SAM@EMAIL.COM"
     @user.email = uppercase_email
