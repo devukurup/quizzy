3e18b8d6e2befc8f7ae43cd62c8766494adcda56
DEVU R KURUP <devukurup199918@gmail.com>
2021-11-04 14:37:38 +0530

    Added role attribute in user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  enum role: %i[standard administrator]
   MAX_LENGTH = 50
   VALID_EMAIL = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
 
