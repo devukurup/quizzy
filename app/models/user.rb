552faba9f66540ee73369e0040ab9c89a525272b
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2021-11-08 10:58:01 +0530

    Changed before_save to before_validation (#30)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   VALID_EMAIL = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   has_secure_password
 
-  before_save :to_email_lowercase
-
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL }
   validates :first_name, presence: true, length: { maximum: MAX_LENGTH }
   validates :last_name, presence: true, length: { maximum: MAX_LENGTH }
   validates :password, length: { minimum: 5 }, if: -> { password.present? }
   validates :password_confirmation, presence: true, on: :create
+
+  before_validation :to_email_lowercase
 
   private
 
