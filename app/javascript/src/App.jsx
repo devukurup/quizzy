812037f623a9d1ccf64980d66057be10592c1906
DEVU R KURUP <devukurup199918@gmail.com>
2021-11-08 19:49:24 +0530

    Added axios headers and defaults

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
+import { setAuthHeaders } from "./apis/axios";
+
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setAuthHeaders(setLoading);
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div>
       <Router>
