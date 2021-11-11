2ed7bc637256238f63dfe706f1640e9046a6e4ce
DEVU R KURUP <devukurup199918@gmail.com>
2021-11-11 06:12:18 +0530

    Added formik validation to login fields

--- app/javascript/src/components/Authentication/Login/index.jsx
+++ app/javascript/src/components/Authentication/Login/index.jsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { Typography, Label, Input, Button } from "@bigbinary/neetoui/v2";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
 
 import authApi from "../../../apis/auth";
 import { setAuthHeaders } from "../../../apis/axios";
 import { setToLocalStorage } from "../../../helpers/storage";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const validationSchema = () => {
+    return Yup.object().shape({
+      email: Yup.string().required("Required").email("Email is invalid"),
+      password: Yup.string().required("Required"),
+    });
+  };
 
-  const handleSubmit = async event => {
-    event.preventDefault();
+  const handleSubmit = data => {
+    handleSubmitRequest(data);
+  };
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const handleSubmitRequest = async data => {
     try {
+      const email = data.email;
+      const password = data.password;
       const response = await authApi.login({ login: { email, password } });
       setToLocalStorage({
         authToken: response.data.authentication_token,
@@ -27,36 +43,46 @@ const Login = () => {
       logger.error(error);
     }
   };
+
   return (
     <div className="flex flex-col justify-center items-center space-y-8 pt-40">
       <Typography style="h1" weight="extrabold">
         Login
       </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div className="space-y-5 ">
+            <div className="grid grid-cols-2">
+              <Label className="mr-5 justify-end">Email :</Label>
+              <Field name="email" type="email">
+                {({ field, meta }) => (
+                  <Input {...field} error={meta.touched && meta.error} />
+                )}
+              </Field>
+            </div>
+            <div className="grid grid-cols-2">
+              <Label className="mr-5 justify-end">Password :</Label>
+              <Field name="password" type="password">
+                {({ field, meta }) => (
+                  <Input
+                    {...field}
+                    type="password"
+                    error={meta.touched && meta.error}
+                  />
+                )}
+              </Field>
+            </div>
 
-      <div className="space-y-5 w-4/12">
-        <div className="grid grid-cols-2">
-          <Label className="mr-5 justify-end">Email :</Label>
-          <Input
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            type="email"
-          />
-        </div>
-        <div className="grid grid-cols-2">
-          <Label className="mr-5 justify-end">Password :</Label>
-          <Input
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </div>
-      </div>
-
-      <Button
-        label="Submit"
-        onClick={handleSubmit}
-        className="neeto-ui-bg-info"
-      />
+            <div>
+              <Button type="submit" label="submit" />
+            </div>
+          </div>
+        </Form>
+      </Formik>
     </div>
   );
 };
