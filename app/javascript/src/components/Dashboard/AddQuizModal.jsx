e8030c03853caf7d92b18c1421b3d216e8f193f2
DEVU R KURUP <devukurup199918@gmail.com>
2021-11-11 07:26:56 +0530

    Added validation to quiz name text field

--- app/javascript/src/components/Dashboard/AddQuizModal.jsx
+++ app/javascript/src/components/Dashboard/AddQuizModal.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { Modal } from "@bigbinary/neetoui/v2";
 import { Typography, Button, Input, Label } from "@bigbinary/neetoui/v2";
+import { Formik, Field, Form } from "formik";
 import Logger from "js-logger";
+import * as Yup from "yup";
 
 import quizzesApi from "../../apis/quizzes";
 
 const AddQuizModal = ({ modal, setModal }) => {
-  const [quizName, setQuizName] = useState("");
+  const validationSchema = () => {
+    return Yup.object().shape({
+      quizName: Yup.string().trim().required("Quiz Name is required"),
+    });
+  };
 
-  const handleSubmit = async event => {
-    const quiz_name = quizName;
-    event.preventDefault();
+  const handleSubmit = data => {
+    handleSubmitRequest(data);
+  };
+
+  const initialValues = {
+    quizName: "",
+  };
+
+  const handleSubmitRequest = async data => {
+    const quiz_name = data.quizName;
     try {
       await quizzesApi.create({ quiz: { quiz_name } });
       setModal(false);
@@ -25,15 +38,27 @@ const AddQuizModal = ({ modal, setModal }) => {
       <Modal.Header className="text-center pb-3">
         <Typography style="h2">Add new quiz</Typography>
       </Modal.Header>
-      <Modal.Body>
-        <div className="flex flex-row">
-          <Label className="mr-5 justify-end">Quiz Name</Label>
-          <Input value={quizName} onChange={e => setQuizName(e.target.value)} />
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button label="Submit" onClick={handleSubmit} />
-      </Modal.Footer>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <Modal.Body>
+            <div className="flex flex-row">
+              <Label className="mr-5 justify-end">Quiz Name</Label>
+              <Field name="quizName" type="text">
+                {({ field, meta }) => (
+                  <Input {...field} error={meta.touched && meta.error} />
+                )}
+              </Field>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button label="Submit" type="submit" />
+          </Modal.Footer>
+        </Form>
+      </Formik>
     </Modal>
   );
 };
