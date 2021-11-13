253add78e1d7e592240d61e1d219dd313b4554f4
DEVU R KURUP <devukurup199918@gmail.com>
2021-11-13 15:41:54 +0530

    Completed 12th issue

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -8,37 +8,37 @@ class QuizzesController < ApplicationController
     render status: :ok, json: { quizzes: quizzes }
   end
 
-  # def create
-  #   @quiz = Quiz.new(quiz_params.merge(quiz_creator_id: current_user.id))
-  #   if @quiz.save
-  #     render status: :ok, json: { notice: "Successfully created new Quiz" }
-  #   else
-  #     errors = @quiz.errors.full_messages.to_sentence
-  #     render status: :unprocessable_entity, json: { error: errors }
-  #   end
-  # end
+  def create
+    @quiz = Quiz.new(quiz_params.merge(quiz_creator_id: current_user.id))
+    if @quiz.save
+      render status: :ok, json: { notice: "Successfully created new Quiz" }
+    else
+      errors = @quiz.errors.full_messages.to_sentence
+      render status: :unprocessable_entity, json: { error: errors }
+    end
+  end
 
-  # def update
-  #   quiz = Quiz.find_by(id: params[:id])
-  #   if quiz && quiz.update(quiz_params)
-  #     render status: :ok, json: { notice: "Successfullly updated quiz." }
-  #   else
-  #     render status: :unprocessable_entity, json: { error: quiz.errors.full_messages.to_sentence }
-  #   end
-  # end
+  def update
+    quiz = Quiz.find_by(id: params[:id])
+    if quiz && quiz.update(quiz_params)
+      render status: :ok, json: { notice: "Successfullly updated quiz." }
+    else
+      render status: :unprocessable_entity, json: { error: quiz.errors.full_messages.to_sentence }
+    end
+  end
 
-  # def destroy
-  #   quiz = Quiz.find_by(id: params[:id])
-  #   if quiz.destroy
-  #     render status: :ok, json: { notice: "Successfully deleted quiz." }
-  #   else
-  #     render status: :unprocessable_entity, json: { error: quiz.errors.full_messages.to_sentence }
-  #   end
-  # end
+  def destroy
+    quiz = Quiz.find_by(id: params[:id])
+    if quiz.destroy
+      render status: :ok, json: { notice: "Successfully deleted quiz." }
+    else
+      render status: :unprocessable_entity, json: { error: quiz.errors.full_messages.to_sentence }
+    end
+  end
 
-  # private
+  private
 
-  #   def quiz_params
-  #     params.require(:quiz).permit(:quiz_name)
-  #   end
+    def quiz_params
+      params.require(:quiz).permit(:quiz_name)
+    end
 end
