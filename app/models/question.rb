945d1b40a0fb5f42b31c486d69708746734ddc7d
DEVU R KURUP <devukurup199918@gmail.com>
2021-11-27 15:02:21 +0530

    Merged two length constants in questions model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
-  MAX_QUESTION_LENGTH = 255
-  MAX_OPTION_LENGTH = 255
+  MAX_LENGTH = 255
 
   belongs_to :quiz
   has_many :attempt_answers, dependent: :destroy
 
-  validates :questn, presence: true, length: { maximum: MAX_QUESTION_LENGTH }
-  validates :option1, presence: true, length: { maximum: MAX_OPTION_LENGTH }
-  validates :option2, presence: true, length: { maximum: MAX_OPTION_LENGTH }
+  validates :questn, presence: true, length: { maximum: MAX_LENGTH }
+  validates :option1, presence: true, length: { maximum: MAX_LENGTH }
+  validates :option2, presence: true, length: { maximum: MAX_LENGTH }
   validates :answer, presence: true
 end
